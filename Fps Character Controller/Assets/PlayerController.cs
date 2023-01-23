b91fe9867918776c5c448baecf58aa1fcdfc3bc7
Tonka <120991424+JunerKs-I@users.noreply.github.com>
2023-01-22 20:44:31 -0400

    Movement script with camera jitter
    The movement is working perfectly, but there's a bug where the camera jitters.
    This mostly happens when you jump.
    It happens because we're moving at a different time than when the camera is rotating. To fix this, we should change the move direction to use a different camera forward instead of the normal one we're using. This controller doesn't use the orientation transform most tutorials do, because it's not necessary with the fix we're going to make.

--- Fps Character Controller/Assets/PlayerController.cs
+++ Fps Character Controller/Assets/PlayerController.cs
@@ -4,28 +4,58 @@ public class PlayerController : MonoBehaviour
 {
     #region Looking variables
 
+    [Header("Looking")]
+    [SerializeField] float mouseSens = 0.2f;
     [SerializeField] Transform cam;
     [SerializeField] Transform body;
-    [SerializeField] float mouseSens = 0.2f;
     float multiplier = 10f;
     float mouseX, mouseY;
     float xRot, yRot;
 
     #endregion
 
+    #region Movement variables
+
+    [Header("Movement")]
+    [SerializeField] float maxSpeed = 7f;
+    [SerializeField] float runAccel = 25f;
+    float accel;
+    Vector3 moveDirection;
+    Vector2 movement;
+    Rigidbody rb;
+
+    [Header("Jumping")]
+    [SerializeField] float jumpForce = 15f;
+    [SerializeField] float distanceToGround = 0.4f;
+    [SerializeField] Transform feetPos;
+    [SerializeField] LayerMask walkableLayer;
+    bool isGrounded;
+
+    [Header("Keys")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    #endregion
+
+    void Awake() => rb = GetComponent<Rigidbody>();
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
 
+        accel = runAccel;
     }
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(feetPos.position, distanceToGround, walkableLayer);
+
         GetMouseInput();
+        GetKeyboardInput();
     }
 
     void FixedUpdate()
     {
-
+        Move();
     }
 
     #region Input
@@ -40,6 +70,31 @@ public class PlayerController : MonoBehaviour
 
         cam.rotation = Quaternion.Euler(xRot, yRot, 0f);
         body.rotation = Quaternion.Euler(0f, yRot, 0f);
+    }
+
+    void GetKeyboardInput()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        moveDirection = cam.forward * movement.y + cam.right * movement.x;
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded) Jump();
+    }
+
+    #endregion
+
+    #region Movement
+
+    void Move()
+    {
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        rb.AddForce(moveDirection.normalized * accel, ForceMode.Force);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     #endregion
